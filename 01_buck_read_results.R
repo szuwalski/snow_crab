1d33ef5dbb89a844abeb9b11accf26d6d5b19c0a
Cody.Szuwalski <cody.szuwalski>
2026-01-20 10:59:30 -0800

    updated GMACS in 25_gmacs_update at January modeling work shop

--- 01_buck_read_results.R
+++ 01_buck_read_results.R
@@ -5,12 +5,12 @@ dirPrj = rstudioapi::getActiveProject()
 #devtools::install_github("wStockhausen/wtsQMD")
 
 ##--identify relative (from project folder) path and name for each model
-fldrs = c( "25_gmacs/","25_gmacs_v2/")
+fldrs = c( "25_gmacs/","25_gmacs_update/")
 cases = names(fldrs)#--model names
 
 #--create full paths, reassign model names
 fldrs=file.path(dirPrj,fldrs) 
-names(fldrs) = c("25.1 gmacs","25.1 gmacs (TBD)")
+names(fldrs) = c("25.1 gmacs","25.1 gmacs (update)")
 
 #--read model results (returns a `gmacs_reslst` object)
 resLst = wtsGMACS::readModelResults(fldrs)
