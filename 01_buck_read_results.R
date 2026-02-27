26a0c134e7304ad90b4b3cb7fb5d83cfda35cff6
Cody.Szuwalski <cody.szuwalski>
2026-02-27 14:14:44 -0800

    updated survey data with hybrid data, added a new folder with model for comparison. added a few data files needed for adding hybrid. script to combine hybrids for data file also included

--- 01_buck_read_results.R
+++ 01_buck_read_results.R
@@ -1,16 +1,25 @@
 #--read model files and create R object
+
+
+# devtools::install_github("wStockhausen/wtsQMD")
+# devtools::install_github("wStockhausen/wtsUtilities")
+# devtools::install_github("wStockhausen/tcsamFunctions")
+# devtools::install_github("wStockhausen/wtsPlots")
+# devtools::install_github("wStockhausen/rTCSAM02")
+# devtools::install_github("wStockhausen/wtsMarkdown")
+# devtools::install_github("wStockhausen/rCompTCMs")
+# devtools::install_github("wStockhausen/wtsGMACS")
+
 require(wtsGMACS)
 dirPrj = rstudioapi::getActiveProject()
 
-#devtools::install_github("wStockhausen/wtsQMD")
-
 ##--identify relative (from project folder) path and name for each model
-fldrs = c( "25_gmacs/","25_gmacs_update/")
+fldrs = c( "25_gmacs/","25_gmacs_update/","25_gmacs_update_hyb_surv")
 cases = names(fldrs)#--model names
 
 #--create full paths, reassign model names
 fldrs=file.path(dirPrj,fldrs) 
-names(fldrs) = c("25.1 gmacs","25.1 gmacs (update)")
+names(fldrs) = c("25.1 gmacs","25.1 gmacs (update)","25.1 gmacs (up + hyb_surv)")
 
 #--read model results (returns a `gmacs_reslst` object)
 resLst = wtsGMACS::readModelResults(fldrs)
