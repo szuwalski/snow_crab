80131f8b24d92e064cece21511a797d2929bd9ff
Grant Adams <adamsgd@uw.edu>
2026-04-12 09:08:59 -0700

    added newmat and plusgroup combo

--- 05_buck_read_results.R
+++ 05_buck_read_results.R
@@ -23,6 +23,8 @@ dirDoc <- dirname(rstudioapi::getActiveDocumentContext()$path)
 model_defs <- c(
   "25.1 gmacs"                     = "25_gmacs/",
   "25.1 gmacs (update)"            = "25_gmacs_update/",
+  "25.1 gmacs (update + plus group)"  = "25_gmacs_update_plus_group",
+  "25.1 gmacs (update + new_mat + plus group)"  = "25_gmacs_update_newmat_plus_group",
   "25.1 gmacs (update + hyb_surv)"          = "25_gmacs_update_hyb_surv/",
   "25.1 gmacs (update + hyb_fishery)"       = "25_gmacs_update_hyb_fishery/",
   "25.1 gmacs (update + hyb_both)"          = "25_gmacs_update_hyb_surv_and_fsh/",
