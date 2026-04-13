35e980c35791659023389f981a88b986c4ffc0db
Grant.Adams <grantadams60091@gmail.com>
2026-04-13 13:50:01 -0700

    adding ssc comments from 2024

--- 05_buck_read_results.R
+++ 05_buck_read_results.R
@@ -23,12 +23,14 @@ dirDoc <- dirname(rstudioapi::getActiveDocumentContext()$path)
 model_defs <- c(
   "25.1 gmacs"                     = "25_gmacs/",
   "25.1 gmacs (update)"            = "25_gmacs_update/",
-  "25.1 gmacs (update + plus group)"  = "25_gmacs_update_plus_group",
-  "25.1 gmacs (update + new_mat + plus group)"  = "25_gmacs_update_newmat_plus_group",
+  "25.1 gmacs (update + compfix)" = "25_gmacs_update_compfix/",
+  "25.1 gmacs (update + plus group)"  = "25_gmacs_update_plus_group/",
+  "25.1 gmacs (update + new_mat + plus group)"  = "25_gmacs_update_newmat_plus_group/",
   "25.1 gmacs (update + hyb_surv)"          = "25_gmacs_update_hyb_surv/",
   "25.1 gmacs (update + hyb_fishery)"       = "25_gmacs_update_hyb_fishery/",
   "25.1 gmacs (update + hyb_both)"          = "25_gmacs_update_hyb_surv_and_fsh/",
   "25.1 gmacs (update + new_mat)"           = "25_gmacs_update_newmat/",
+  "25.1 gmacs (update + new_mat + compfix)" = "25_gmacs_update_newmat_compfix/",
   "25.1 gmacs (update + hyb_both_new_mat)"  = "25_gmacs_update_hyb_surv_and_fsh_newmat/"
 )
 
