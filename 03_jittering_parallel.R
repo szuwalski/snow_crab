3402baece00bdc6ca7e23dce05dd7e5e5f974d73
Grant.Adams <grantadams60091@gmail.com>
2026-04-13 15:24:12 -0700

    fix names of files

--- 03_jittering_parallel.R
+++ 03_jittering_parallel.R
@@ -1,110 +1,123 @@
-#==script for jittering
-#library(gmr)
+# Load necessary libraries
 library(doParallel)
-library(parallel)
 library(foreach)
 library(ggplot2)
+library(dplyr)
+library(tidyr)
 
-# Detect the number of available cores and create cluster
-use_cores<-detectCores()-2
+# --- 1. Setup Environment ----
+use_cores <- parallel::detectCores() - 2
 cl <- parallel::makeCluster(use_cores)
 doParallel::registerDoParallel(cl)
-orig_drv<-c("25_gmacs")
 
-tot_it<-100
-orig_wd<-getwd()
-z<-1 # add loop for more directories...
-for(z in 1:length(orig_drv))
-{
-  work_drv<-paste(orig_drv[z],"/jitter/",sep='')
-  dir.create(work_drv)
+# Define scenarios (using your requested snow/gmacs replacement logic)
+orig_drv <- c("25_gmacs_update_hyb_surv_and_fsh_newmat", 
+              "25_gmacs_update_hyb_surv_and_fsh", 
+              "25_gmacs_update_plus_group", 
+              "25_gmacs_update_newmat_plus_group")
+
+tot_it  <- 100
+orig_wd <- getwd()
+
+
+# --- 2. Jittering Execution ----
+for(d in seq_along(orig_drv)) {
+  current_scenario <- orig_drv[d]
+  jitter_root <- file.path(current_scenario, "jitter")
   
- foreach(x = 1:tot_it)%dopar%
-  {
-    #=make new drive
-    work_drv<-paste(orig_drv[z],"/jitter/",x,sep='')
-    dir.create(work_drv)
-
-    #=copy needed files
-    file.copy(paste(orig_drv[z],"/snow.dat",sep=""),
-              work_drv)
-    file.copy(paste(orig_drv[z],"/snow.ctl",sep=""),
-              work_drv)
-    file.copy(paste(orig_drv[z],"/snow.prj",sep=""),
-              work_drv)
-    file.copy(paste(orig_drv[z],"/gmacs.dat",sep=""),
-              work_drv)
-    file.copy(paste(orig_drv[z],"/gmacs.exe",sep=""),
-              work_drv)
+  if (!dir.exists(jitter_root)) dir.create(jitter_root, recursive = TRUE)
+  
+  message("Starting jitter for: ", current_scenario)
+  
+  # Parallel loop for iterations
+  foreach(i = 1:tot_it, .packages = NULL) %dopar% {
+    work_dir <- file.path(jitter_root, as.character(i))
+    if (!dir.exists(work_dir)) dir.create(work_dir)
     
-    in_proj<-readLines(paste(orig_drv[z],"/gmacs.dat",sep=""))
-    in_proj[grep("Jitter",in_proj)+1]<-"1 0 0.1"
+    # Define files to copy
+    dat_name <- paste0(gsub("gmacs", "snow", current_scenario), ".dat")
+    
+    files_to_copy <- c(
+      file.path(current_scenario, dat_name),
+      file.path(current_scenario, "snow.ctl"),
+      file.path(current_scenario, "snow.prj"),
+      file.path(current_scenario, "gmacs.dat"),
+      file.path(current_scenario, "gmacs.exe")
+    )
+    
+    file.copy(files_to_copy, work_dir, overwrite = TRUE)
+    
+    # Modify gmacs.dat to enable jitter
+    gmacs_dat_path <- file.path(work_dir, "gmacs.dat")
+    in_proj <- readLines(gmacs_dat_path)
+    # Update Jitter line (assuming standard GMACS format)
+    jitter_idx <- grep("Jitter", in_proj)
+    if(length(jitter_idx) > 0) in_proj[jitter_idx + 1] <- "1 0 0.1"
+    writeLines(in_proj, gmacs_dat_path)
 
-    #==rerun assessment
-    setwd(paste(work_drv))
-    write(in_proj,file="gmacs.dat")
-    system("gmacs.exe")
-    #system("gmacs.exe")
-    #==do until...what exactly?
+    # Run assessment using shell/system call
+    # Using 'shell' on Windows or 'system' on Unix; cd into dir first
+    cmd <- paste0("cmd /c \"cd /d ", work_dir, " && gmacs.exe\"")
+    system(cmd, show.output.on.console = FALSE)
   }
 }
 
-setwd(orig_wd)
-#==read in gmacs output
-#==compare likelihoods
-#==compare derived quantities
-#==compare parameter estimatesif(!is.na(mod_names[1]))
+# Clean up cluster
+parallel::stopCluster(cl)
+closeAllConnections()
 
-keep_out<-matrix(nrow=tot_it,ncol=9)
-for(z in 1:tot_it)
-{
-  mod_dir  <-paste(orig_drv,"jitter/",z,"/",sep="") 
-  take_row<-c(1,2,3,4,8,12,13)
-  colnames(keep_out)<-seq(1,ncol(keep_out))
-  if(file.exists(paste(mod_dir,"Gmacsall.out",sep='')))
-  {
-    tmp<-readLines(paste(mod_dir,"Gmacsall.out",sep=''))
-    st<-grep("BMSY",tmp)[1]
-    end<-grep("Ofl",tmp)[length(grep("Ofl",tmp))]
-    use<-tmp[st:end]
+
+# --- 3. Extract Results ---
+extract_jitter_results <- function(scenario_path, n_iterations) {
+  all_data <- list()
+  
+  for(i in 1:n_iterations) {
+    out_file <- file.path(scenario_path, "jitter", i, "Gmacsall.out")
     
-    keep_out[z,9]<-as.numeric(unlist(strsplit(tmp[grep("Total",tmp)[1]],split=" "))[2])
-    for(x in 1:length(take_row))
-    {
-      if(x<4)
-        keep_out[z,x]<-as.numeric(strsplit(use[take_row[x]],split=" ")[[1]][3])
-      if(x>=4)
-        keep_out[z,x]<-as.numeric(strsplit(use[take_row[x]],split=" ")[[1]][4])
-      colnames(keep_out)[x]<-strsplit(use[take_row[x]],split=" ")[[1]][1]
+    if (file.exists(out_file)) {
+      tmp <- readLines(out_file)
+      
+      # Extract Neg Log Likelihood (Total)
+      nll_line <- tmp[grep("Total", tmp)[1]]
+      nll_val  <- as.numeric(unlist(strsplit(nll_line, " +"))[2])
+      
+      # Extract Management Quantities
+      st  <- grep("BMSY", tmp)[1]
+      end <- grep("Ofl", tmp)[length(grep("Ofl", tmp))]
+      use <- tmp[st:end]
+      
+      # Helper to parse the weirdly formatted GMACS output lines
+      parse_gmacs_line <- function(lines, pattern, index) {
+        line <- lines[grep(pattern, lines)]
+        as.numeric(unlist(strsplit(line, " +"))[index])
+      }
+      
+      all_data[[i]] <- data.frame(
+        scenario   = scenario_path,
+        jitter     = i,
+        negloglike = nll_val,
+        BMSY       = parse_gmacs_line(use, "BMSY", 3),
+        Status     = parse_gmacs_line(use, "Status", 3),
+        OFL        = parse_gmacs_line(use, "OFL", 4), # Directed OFL
+        FMSY       = parse_gmacs_line(use, "FMSY", 3),
+        FOFL       = parse_gmacs_line(use, "FOFL", 4)
+      )
     }
-    }
-    keep_out[z,8]<-z
+  }
+  return(bind_rows(all_data))
 }
 
-colnames(keep_out)<-c("BMSY","Status","tot_ofl","FMSY","FOFL","OFL","disc_OFL","jitter","negloglike")
+# Run extraction for all scenarios
+jit_summary <- bind_rows(lapply(orig_drv, extract_jitter_results, n_iterations = tot_it))
 
-# check nll
-# keep_out[order(keep_out[,9]), ]
-png('plots/jittered_ofl_95.png',height=6,width=6,res=350,units='in')
-ggplot()+
-  geom_point(data=as.data.frame(keep_out),aes(x=negloglike,y=OFL))+
-  theme_bw()+
-  ylab("OFL (1,000 t)")+
-    xlim(-19250,-18800)
+
+# --- 4. Plotting ----
+png('plots/jittered_results_all.png', height = 8, width = 10, res = 300, units = 'in')
+ggplot(jit_summary, aes(x = negloglike, y = OFL, color = scenario)) +
+  geom_point(alpha = 0.6) +
+  facet_wrap(~scenario, scales = "free") +
+  theme_bw() +
+  labs(title = "Jitter Convergence Check", 
+       x = "Negative Log-Likelihood", 
+       y = "OFL (1,000 t)")
 dev.off()
-
-
-#======================
-# look at parameters
-#=====================
-hold_pars<-matrix(ncol=262,nrow=tot_it)
-for(z in 1:tot_it)
-{
-tmp<-readLines(paste(orig_drv,"/jitter/",z,"/gmacs.par",sep=""))
-hold_pars[z,]<-tmp[-grep('#',tmp)]  
-} 
-colnames(hold_pars)<-tmp[grep('#',tmp)[-1]] 
-  
-library(dplyr)  
-plot(as.numeric(hold_pars[,1])~filter(jit_out,model=="24.1c")$nlogl)
-plot(as.numeric(hold_pars[,2])~filter(jit_out,model=="24.1c")$nlogl)
