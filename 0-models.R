a0d3c326382874f74834c257567068917a325c4f
Grant.Adams <grantadams60091@gmail.com>
2026-04-24 10:10:32 -0700

    more updates for VM

--- 0-models.R
+++ 0-models.R
@@ -7,12 +7,14 @@ model_defs <- c(
   "25.1 gmacs (update + imm_surv + compfix)" = "25_gmacs_update_imm_compfix/",
   "25.1 gmacs (update + imm_surv + plus group)" = "25_gmacs_update_imm_plus_group/",
   "25.1 gmacs (update + compfix)"           = "25_gmacs_update_compfix/",
-  "25.1 gmacs (update + plus group)"        = "25_gmacs_update_plus_group/",
-  "25.1 gmacs (update + new_mat + plus group)"  = "25_gmacs_update_newmat_plus_group/",
+  "25.1 gmacs (update + plus group)"        = "25_gmacs_update_plus_group/", # Jittered
+  "25.1 gmacs (update + new_mat + plus group)"  = "25_gmacs_update_newmat_plus_group/", # Jittered
+  
+  "25.1 gmacs (update + new_mat + imm_surv + plus group)" = "25_gmacs_update_new_mat_imm_plus_group/",
   "25.1 gmacs (update + hyb_surv)"          = "25_gmacs_update_hyb_surv/",
   "25.1 gmacs (update + hyb_fishery)"       = "25_gmacs_update_hyb_fishery/",
-  "25.1 gmacs (update + hyb_both)"          = "25_gmacs_update_hyb_surv_and_fsh/",
+  "25.1 gmacs (update + hyb_both)"          = "25_gmacs_update_hyb_surv_and_fsh/", # Jittered
   "25.1 gmacs (update + new_mat)"           = "25_gmacs_update_newmat/",
   "25.1 gmacs (update + new_mat + compfix)" = "25_gmacs_update_newmat_compfix/",
-  "25.1 gmacs (update + hyb_both_new_mat)"  = "25_gmacs_update_hyb_surv_and_fsh_newmat/"
+  "25.1 gmacs (update + hyb_both + new_mat)"  = "25_gmacs_update_hyb_surv_and_fsh_newmat/" # Jittered
 )
