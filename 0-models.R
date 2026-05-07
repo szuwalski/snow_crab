422ec2416db2255cc774d2fbe2886048aa02a0c2
Grant.Adams <grantadams60091@gmail.com>
2026-05-06 17:29:30 -0700

    doc updates

--- 0-models.R
+++ 0-models.R
@@ -2,20 +2,20 @@
 # Using a named vector directly ensures each folder has a unique label
 # and prevents length mismatches.
 model_defs <- c(
-  "25.1 gmacs"                     = "25_gmacs/",
-  "25.1 gmacs (update)"            = "25_gmacs_update/",
-  "25.1 gmacs (update + compfix)"           = "25_gmacs_update_compfix/",
-  "25.1 gmacs (update + compfix + plus group)"        = "25_gmacs_update_plus_group/", # Jittered
-  "25.1 gmacs (update + compfix + imm_surv)" = "25_gmacs_update_imm_compfix/",
-  "25.1 gmacs (update + compfix + plus group + imm_surv)" = "25_gmacs_update_imm_plus_group/", # Jittered
-  "25.1 gmacs (update + new_mat)"           = "25_gmacs_update_newmat/",
-  "25.1 gmacs (update + compfix + new_mat)" = "25_gmacs_update_newmat_compfix/",
-  "25.1 gmacs (update + compfix + plus group + new_mat)"  = "25_gmacs_update_newmat_plus_group/", # Jittered
-  "25.1 gmacs (update + compfix + plus group + new_mat + imm_surv)" = "25_gmacs_update_newmat_imm_plus_group/", # Jittered
-  "25.1 gmacs (update + compfix + plus group + hyb_fishery)"       = "25_gmacs_update_hyb_fishery/",
-  "25.1 gmacs (update + compfix + plus group + hyb_surv)"          = "25_gmacs_update_hyb_surv/",
-  "25.1 gmacs (update + compfix + plus group + hyb_both)"          = "25_gmacs_update_hyb_surv_and_fsh/", # Jittered
-  "25.1 gmacs (update + compfix + plus group + hyb_both + new_mat)"  = "25_gmacs_update_hyb_surv_and_fsh_newmat/" # Jittered
+  "25.1 gmacs"                                                                              = "25_gmacs/",
+  "25.1 gmacs (update)"                                                               = "25_gmacs_update/",
+  "25.1 gmacs (update + compfix)"                                                    = "25_gmacs_update_compfix/",
+  "25.1 gmacs (update + compfix + plus group)"                    = "25_gmacs_update_plus_group/", # Jittered
+  "25.1 gmacs (update + compfix + imm_surv)"       = "25_gmacs_update_imm_compfix/",
+  "25.1 gmacs (update + compfix + plus group + imm_surv)"       = "25_gmacs_update_imm_plus_group/", # Jittered
+  "25.1 gmacs (update + new_mat)"                           = "25_gmacs_update_newmat/",
+  "25.1 gmacs (update + compfix + new_mat)"       = "25_gmacs_update_newmat_compfix/",
+  "25.1 gmacs (update + compfix + plus group + new_mat)"         = "25_gmacs_update_newmat_plus_group/", # Jittered
+  "25.1 gmacs (update + compfix + plus group + new_mat + imm_surv)"      = "25_gmacs_update_newmat_imm_plus_group/", # Jittered
+  "25.1 gmacs (update + compfix + plus group + hyb_fishery)"                   = "25_gmacs_update_hyb_fishery/",
+  "25.1 gmacs (update + compfix + plus group + hyb_surv)"                          = "25_gmacs_update_hyb_surv/",
+  "25.1 gmacs (update + compfix + plus group + hyb_both)"                          = "25_gmacs_update_hyb_surv_and_fsh/", # Jittered
+  "25.1 gmacs (update + compfix + plus group + hyb_both + new_mat)"         = "25_gmacs_update_hyb_surv_and_fsh_newmat/" # Jittered
 )
 
 
@@ -27,20 +27,20 @@ model_defs <- c(
 # pg = plus group, imm = immature survey index, nm = new_mat,
 # hs = hyb_surv, hf = hyb_fishery, hb = hyb_both.
 model_shorts <- c(
-  "25.1 gmacs"                                                       = "25.1",
-  "25.1 gmacs (update)"                                              = "u",
-  "25.1 gmacs (update + compfix)"                                    = "u+cf",
-  "25.1 gmacs (update + compfix + plus group)"                       = "u+cf+pg", # Jittered
-  "25.1 gmacs (update + compfix + imm_surv)"                         = "u+cf+imm",
-  "25.1 gmacs (update + compfix + plus group + imm_surv)"            = "u+cf+pg+imm", # Jittered
-  "25.1 gmacs (update + new_mat)"                                    = "u+nm",
-  "25.1 gmacs (update + compfix + new_mat)"                          = "u+cf+nm/",
-  "25.1 gmacs (update + compfix + plus group + new_mat)"             = "u+cf+pg+nm", # Jittered
-  "25.1 gmacs (update + compfix + plus group + new_mat + imm_surv)"  = "u+cf+pg+imm+nm", # Jittered
-  "25.1 gmacs (update + compfix + plus group + hyb_fishery)"         = "u+cf+pg+hf",
-  "25.1 gmacs (update + compfix + plus group + hyb_surv)"            = "u+cf+pg+hs",
-  "25.1 gmacs (update + compfix + plus group + hyb_both)"            = "u+cf+pg+hb", # Jittered
-  "25.1 gmacs (update + compfix + plus group + hyb_both + new_mat)"  = "u+cf+pg+hb+nm" # Jittered
+  "25.1 gmacs"                                                        =  "Model 25"   , 
+  "25.1 gmacs (update)"                                               =  "Model 25.1a", 
+  "25.1 gmacs (update + compfix)"                                     =  "Model 25.1b", 
+  "25.1 gmacs (update + compfix + plus group)"        =  "Model 25.1c", # Jittered
+  "25.1 gmacs (update + compfix + imm_surv)"  =  "Model 25.1d", 
+  "25.1 gmacs (update + compfix + plus group + imm_surv)"  =  "Model 25.1e", # Jittered
+  "25.1 gmacs (update + new_mat)"            =  "Model 25.2a", 
+  "25.1 gmacs (update + compfix + new_mat)"  =  "Model 25.2b", 
+  "25.1 gmacs (update + compfix + plus group + new_mat)"   =  "Model 25.2c", # Jittered
+  "25.1 gmacs (update + compfix + plus group + new_mat + imm_surv)" =  "Model 25.2d", # Jittered
+  "25.1 gmacs (update + compfix + plus group + hyb_fishery)"        =  "Model 25.3a", 
+  "25.1 gmacs (update + compfix + plus group + hyb_surv)"           =  "Model 25.3b", 
+  "25.1 gmacs (update + compfix + plus group + hyb_both)"           =  "Model 25.3c", # Jittered
+  "25.1 gmacs (update + compfix + plus group + hyb_both + new_mat)"   =  "Model 25.3d"  # Jittered
 )
 
 # Sanity check: every model in model_defs needs an entry in model_shorts
